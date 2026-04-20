737f302e22b13b0b8fd4eb1d4c976fd605e3bac3
FabricaIdeas-eNeOe <noe.ruizpuon@gmail.com>
2026-04-19 20:14:24 -0600

    Update images.js

--- api/images.js
+++ api/images.js
@@ -1,43 +1,48 @@
 export default async function handler(req, res) {
-
   try {
-
     const cloud = "du4pmuch7";
     const apiKey = process.env.CLOUDINARY_API_KEY;
     const apiSecret = process.env.CLOUDINARY_API_SECRET;
 
     const folder = req.query.folder;
 
-    const url = `https://api.cloudinary.com/v1_1/${cloud}/resources/search`;
+    let allImages = [];
+    let nextCursor = null;
 
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Authorization": "Basic " + btoa(apiKey + ":" + apiSecret),
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        expression: `folder:${folder}`,
-        max_results: 500
-      })
-    });
+    do {
+      const url = `https://api.cloudinary.com/v1_1/${cloud}/resources/search`;
 
-    const data = await response.json();
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          Authorization:
+            "Basic " + Buffer.from(apiKey + ":" + apiSecret).toString("base64"),
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          expression: `folder:${folder}`,
+          max_results: 500,
+          next_cursor: nextCursor,
+        }),
+      });
 
-    if (!data.resources) {
-      return res.status(500).json(data);
-    }
+      const data = await response.json();
 
-    const images = data.resources.map(r => r.public_id);
+      if (!data.resources) {
+        return res.status(500).json(data);
+      }
+
+      allImages = allImages.concat(data.resources);
+
+      nextCursor = data.next_cursor || null;
+    } while (nextCursor);
+
+    const images = allImages.map((r) => r.public_id);
 
     res.status(200).json(images);
-
   } catch (err) {
-
     res.status(500).json({
-      error: err.message
+      error: err.message,
     });
-
   }
-
 }
